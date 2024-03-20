7a11cf459d80e0ab0f201aa8d4a43372be466fa5
brentnettles <brentnettles@gmail.com>
2024-03-20 15:13:42 -0400

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+
+
+items = ['guitar', "bass", "drums", "sax", "bongos"]
+
+oxford_comma(items)
+print(len(items))
+print(items[0:3])
